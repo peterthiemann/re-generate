9b7ce8e3654c5bf37545e309a21d86c1a1280687
Peter Thiemann <peter.thiemann@gmail.com>
2018-03-09 22:16:07 +0100

    switch between algorithms

--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,19 @@ import Control.Monad.IO.Class
 import System.TimeIt as T
 
 import GRegexp
+import qualified GenRefined as GR
 import qualified GenSegments as GS1
 import qualified GenString as G
 import RegexParser
 
+data Backend
+  = Seg
+  | Ref
+  deriving (Read, Show, Enum, Bounded)
+
 data Regen = Regen 
   { negate :: Bool
+  , backend :: Backend
   , alphabet :: String
   , maxlength :: Maybe Int
   , quiet :: Bool
@@ -28,6 +35,12 @@ regen = Regen
           ( long "complement"
          <> short 'c'
          <> help "generate language complement" )
+      <*> option auto
+          ( long "backend"
+          <> short 'b'
+          <> metavar "BACKEND"
+          <> value Seg
+          <> help ("BACKEND algorithm used for generator: " ++ show [(minBound::Backend) .. maxBound]))
       <*> strOption
           ( long "alphabet"
             <> short 'a'
@@ -59,8 +72,12 @@ main = greet =<< execParser opts
      <> progDesc "blablabla"
      <> header "Generate test inputs for a regular expression" )
 
+select :: (Ord t) => Backend -> [t] -> GRE t -> [[[ t ]]]
+select Seg = GS1.generate'
+select Ref = GR.generate'
+
 greet :: Regen -> IO ()
-greet (Regen c sigma mm q t r) = do
+greet (Regen c b sigma mm q t r) = do
   unless q $ putStrLn $ "Called with complement = " ++ show c ++ ", maxlength = " ++ show mm ++ ", timing = " ++ show t ++ ", REGEXP = " ++ r
   case parseRe r of
     Nothing ->
@@ -68,7 +85,7 @@ greet (Regen c sigma mm q t r) = do
     Just gre -> do
       let
         gre' = if c then Not gre else gre
-        result = GS1.generate' sigma gre'
+        result = select b sigma gre'
         output2 = case mm of
                     Nothing -> result
                     Just m  -> take (m+1) result

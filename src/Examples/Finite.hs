d20dadd8270fcc8d1225fb64603d7f9abcfa6790
Peter Thiemann <peter.thiemann@gmail.com>
2018-03-14 17:31:52 +0100

    introduced list comprehensions

--- src/Examples/Finite.hs
+++ src/Examples/Finite.hs
@@ -26,7 +26,5 @@ star lx = take limit $ removeDuplicates $ concat lxs
 complement :: Alphabet -> Lang -> Lang
 complement sigma lx = take limit (concat lsigmastar L.\\ lx) 
   where
-    lsigmastar =
-      [T.empty] : 
-      map (\lsigmai -> concatMap (\la -> concatenate la lsigmai) lsigma1) lsigmastar
-    lsigma1 = map (return . T.singleton) sigma
+    lsigmastar = [T.empty] : map extend lsigmastar
+    extend lsigmai = [T.cons a w | a <- sigma, w <- lsigmai]

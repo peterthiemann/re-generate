54f1378b035dd207030a2603f4614a82647d6850
Peter Thiemann <peter.thiemann@gmail.com>
2018-03-15 12:26:26 +0100

    refined implementation that preserves finiteness in expressions without complementation

--- src/Examples/Segmented.hs
+++ src/Examples/Segmented.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE TypeFamilies, FlexibleInstances #-}
 module Examples.Segmented where
 
+import Control.Applicative
+
 import qualified Data.Text as T
 import qualified Examples.IncreasingListOps as ILO
 import GREImpl
 
-instance GREImpl [[T.Text]] where
-  type Sym [[T.Text]] = Char
-  type Wrd [[T.Text]] = T.Text
+type SegLang = [[T.Text]]
+
+instance GREImpl SegLang where
+  type Sym SegLang = Char
+  type Wrd SegLang = T.Text
 
   toList = concat
 
@@ -23,13 +27,28 @@ instance GREImpl [[T.Text]] where
   intersect lx [] = []
   intersect (sx:lx) (sy:ly) = ILO.intersect sx sy : intersect lx ly
   
-  concatenate lx ly = collect 0
+  concatenate lx ly =
+    collect lx ly Nothing Nothing 0
     where
-      collect n =
-        (foldr ILO.union [] $ map (combine n) [0 .. n]) : collect (n+1)
+      collect xss yss mmx mmy n =
+        let (xss', mmx') = updateMax xss mmx n
+            (yss', mmy') = updateMax yss mmy n
+            mbound = liftA2 (+) mmx mmy
+        in
+          case mbound of
+            Just m | n > m ->
+                     []
+            _ ->
+              (foldr ILO.union [] $ map (combine n) [0 .. n]) :
+              collect xss' yss' mmx' mmy' (n+1)
       combine n i =
         [T.append x y | x <- lx !!! i, y <- ly !!! (n - i)]
+      updateMax _ mm@(Just _) n = ([], mm)
+      updateMax [] Nothing n = ([], Just n)
+      updateMax (_:xss) Nothing n = (xss, Nothing)
 
+  star [] = one
+  star [_] = one
   star lx = lr
     where
       lr = [T.empty] : collect 1
@@ -47,6 +66,13 @@ difference [] ly = []
 difference lx [] = lx
 difference (sx:lx) (sy:ly) = ILO.difference sx sy : difference lx ly
 
+concatenate' :: SegLang -> SegLang -> SegLang
+concatenate' lx ly = collect 0
+  where
+    collect n =
+      (foldr ILO.union [] $ map (combine n) [0 .. n]) : collect (n+1)
+    combine n i =
+      [T.append x y | x <- lx !!! i, y <- ly !!! (n - i)]
 
 -- | list index beyond the end of the list
 (!!!) :: [[a]] -> Int -> [a]
